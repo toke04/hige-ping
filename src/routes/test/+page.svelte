e462046ba1f19f4618cf6f56ceea98287f0d37e2
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-11-09 14:51:51 +0900

    svelte:windowタグを利用して、ボタンを押した時にそのアルファベットを取得するようにした

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,27 +1,34 @@
 <script>
 	let count = 0;
-	const inCrementCount = () => {
-		count += 1;
-	};
-	const deCrementCount = () => {
-		count -= 1;
+	let text = 'hello';
+	let input = '';
+	// const handleKey = (e: React.KeyboardEvent<HTMLDivElement>) => {
+	// 	// 入力したキーと現在入力しようとしている文字が一致するとき
+	// 	if (e.key === text[position]) {
+	// 		// 次の位置へ移動
+	// 		setPosition(position + 1);
+	// 	}
+	// };
+	const handleKeydown = (e) => {
+		input = e.key;
 	};
 </script>
-
+<svelte:window on:keydown={handleKeydown} />
 <svelte:head>
 	<title>test</title>
 	<meta name="description" content="test this app" />
 </svelte:head>
 
 <div>
-	<h1>ああ</h1>
-	<h1>{count}</h1>
-	<p><button on:click={inCrementCount}>Increment</button></p>
-	<p>
-		<button
-			on:click={deCrementCount}
-			class="mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-			>Increment</button
-		>
-	</p>
+	<h1>{ text }</h1>
+	<h1>{ input }</h1>
+<!--	<p><button on:keydown={handleKey}>ボタンを押して</button></p>-->
+
+<!--	<p>-->
+<!--		<button-->
+<!--			on:click={deCrementCount}-->
+<!--			class="mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"-->
+<!--			>Increment</button-->
+<!--		>-->
+<!--	</p>-->
 </div>
