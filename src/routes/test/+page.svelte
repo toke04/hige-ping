82bc042a515ea0ade47d13d30602a7b8e7b6a769
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-11-09 13:42:13 +0900

    scriptタグにreactみたいに変数宣言したらデータバインドできること確認した

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+	let count = 0;
+	const inCrementCount = () => {
+		count += 1;
+	};
+	const deCrementCount = () => {
+		count -= 1;
+	};
+</script>
+
 <svelte:head>
 	<title>test</title>
 	<meta name="description" content="test this app" />
@@ -5,4 +15,7 @@
 
 <div>
 	<h1>ああ</h1>
+	<h1> {count} </h1>
+	<p><button on:click={inCrementCount}>Increment</button></p>
+	<p><button on:click={deCrementCount}>Increment</button></p>
 </div>
