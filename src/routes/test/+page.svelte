98a0eefec2ca2f53b479685a92da92575fb191f9
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-11-09 14:08:45 +0900

    cssフレームワークの代わりにtailwind使ってスタイル当てるように変更した

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -15,7 +15,13 @@
 
 <div>
 	<h1>ああ</h1>
-	<h1> {count} </h1>
+	<h1>{count}</h1>
 	<p><button on:click={inCrementCount}>Increment</button></p>
-	<p><button on:click={deCrementCount}>Increment</button></p>
+	<p>
+		<button
+			on:click={deCrementCount}
+			class="mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+			>Increment</button
+		>
+	</p>
 </div>
