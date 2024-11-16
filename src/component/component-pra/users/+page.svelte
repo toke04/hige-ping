d564749d144eb9edee613d85eb7c59a8d51d3a49
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-11-16 15:16:51 +0900

    親コンポーネントから値を渡すパターンも試した。受け取りはexportが必要

---
+++ src/component/component-pra/users/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let users = {};
+</script>
+
+<ul>
+	{#each users as user}
+		<li>名前: {user.name} 年齢: {user.age}</li>
+	{/each}
+</ul>
--- src/routes/component-pra/+page.svelte
+++ src/routes/component-pra/+page.svelte
@@ -1,7 +1,13 @@
 <script>
 	import Child from '../../component/component-pra/child1/+page.svelte';
 	import Books from '../../component/component-pra/books/+page.svelte';
+	import Users from '../../component/component-pra/users/+page.svelte'
 	let props = '渡す'
+	let users = [
+		{name: "taro", age: 18},
+		{name: "hanako", age: 49},
+		{name: "tomo", age: 37},
+	]
 </script>
 
 <div>
@@ -10,9 +16,10 @@
 	<Child />
 	<Child name={props}/>
 	<div class="mt-4">
-		<ul>
-			<Books/>
-		</ul>
+		<Books/>
+		<div class="mt-4">
+			<Users users={users}/>
+		</div>
 	</div>
 
 </div>
